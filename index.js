9f686f981605674d00f85013fd3b247cf760136e
desertdweller15 <bcoutee11@gmail.com>
2015-07-10 12:43:03 -0700

    7.10.15 upload2

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+    $("img").fadeOut(2000)
+    });
+
+$(document).ready(function(){
+    $("button").click(function(){
+    $("img").fadeIn(2000)
+    });
